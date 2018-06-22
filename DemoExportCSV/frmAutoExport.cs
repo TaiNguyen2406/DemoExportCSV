312bb7b9a9ff76c80df14a0e41544d305ec6bb0a
underwaterhp93 <underwaterhp93@gmail.com>
2018-06-22 15:09:35 +0700

    Add project files.

---
+++ DemoExportCSV/frmAutoExport.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoExportCSV
+{
+    public partial class frmAutoExport : Form
+    {
+        public frmAutoExport()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgv.DataSource = TaoBang();
+            dgv.Columns[0].Width = 80;
+            dgv.Columns[1].Width = 80;
+            for (int i=2;i<dgv.ColumnCount -1;i++)
+            {
+                dgv.Columns[i].Width = 40;
+            }
+        }
+        static int seed = Environment.TickCount;
+        static readonly ThreadLocal<Random> random =
+            new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref seed)));
+        private DataTable TaoBang()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Date", typeof(String));
+            dt.Columns.Add("Time", typeof(String));
+            for (int i = 0; i < 4; i++)
+            {
+                dt.Columns.Add("nhietdo" + i, typeof(Decimal));
+            }
+            for (int i = 4; i <= 6; i++)
+            {
+                for (int j = 1; j <= 20; j++)
+                {
+                    dt.Columns.Add(i + "L" + j, typeof(Decimal));
+                }
+                for (int j = 1; j <= 20; j++)
+                {
+                    dt.Columns.Add(i + "R" + j, typeof(Decimal));
+                }
+            }
+            dt.Rows.Add(dt.NewRow());
+            dt.Rows[0]["Date"] = DateTime.Now.ToString("dd/MM/yyyy");
+            dt.Rows[0]["Time"] = DateTime.Now.ToString("HH:mm:ss");
+            for (int i = 2; i <= dt.Columns.Count -1; i++)
+            {
+                Random rnd = new Random();
+                if (i<=5)
+                {
+                    dt.Rows[0][i] = random.Value.Next(1000, 10000);
+                }
+                else
+                {
+                    dt.Rows[0][i] = random.Value.Next(0, 500);
+                }
+               
+            }
+                return dt;
+        }
+        public void Export(DataTable dt)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+                                              Select(column => column.ColumnName);
+            sb.AppendLine(string.Join(",", columnNames));
+
+            foreach (DataRow row in dt.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field =>
+                  string.Concat("\"", field.ToString().Replace("\"", "\"\""), "\""));
+                sb.AppendLine(string.Join(",", fields));
+
+            }
+            //modify
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "CSV|*.csv|All file|*.*";
+            save.FilterIndex = 1;
+            DateTime hientai = DateTime.Now;
+            Directory.CreateDirectory("D:/csv/" + hientai.Month.ToString());
+            File.WriteAllText("D:/csv/"+ hientai.Month.ToString()+"/"+ hientai.ToString("dd-MM-yyyy HHmmss")+".csv", sb.ToString());
+            for(int i=1;i<=12;i++)
+            {
+                if (i!= hientai.Month)
+                {
+                    string filePath = "D:/csv/" + i.ToString();
+                    if (Directory.Exists(filePath))
+                    {
+                        Directory.Delete(filePath, true);
+                    }
+                    
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = TaoBang();
+            dgv.DataSource = dt;
+            Export(dt);
+        }
+    }
+}
---
+++ DemoExportCSV/frmDocDuLieu.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+namespace DemoExportCSV
+{
+    public partial class frmDocDuLieu : Form
+    {
+        ModbusClient modbusClient;
+        public frmDocDuLieu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDocDuLieu_Load(object sender, EventArgs e)
+        {
+            txtIP.Text = GetIPAddress();
+
+        }
+        public static string GetIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                }
+            }
+            return localIP;
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                modbusClient = new ModbusClient(txtIP.Text, 502);    //Ip-Address and Port of Modbus-TCP-Server
+                modbusClient.Connect();
+                lblStatus.Text = "Connected";
+                timer2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                timer2.Enabled = false;
+                lblStatus.Text = ex.ToString();
+                throw;
+            }
+        }
+
+        private void btnDisConnect_Click(object sender, EventArgs e)
+        {
+            modbusClient.Disconnect();
+            lblStatus.Text = "Disconnected";
+            timer2.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            bool[] readCoils = modbusClient.ReadCoils(0, 10);                        //Read 10 Coils from Server, starting with address 0
+            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(0, 10);
+            txtGiaTri1.Text = readHoldingRegisters[0].ToString ();
+            txtGiaTri2.Text = readHoldingRegisters[1].ToString();
+            chk1.Checked = readCoils[0];
+            chk2.Checked = readCoils[1];
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          //  modbusClient.WriteMultipleCoils(4, new bool[] { true, false, true, true, true, true, true, true, true, true });    //Write 10 Coils starting with Address 5, ghi nhiều ô coil 1 lần
+         //   modbusClient.WriteSingleCoil(20, true); // ghi 1 ô coil 1 lần
+         //   modbusClient.WriteMultipleRegisters(0, new int[] { 1, 2, 3, 4, 5, 6 }); //ghi nhiều ô register 1 lần
+            modbusClient.WriteSingleRegister(Decimal.ToInt32(nudRegNoW.Value), Decimal.ToInt32( nudRegValue.Value));
+            modbusClient.WriteSingleCoil(Decimal.ToInt32(nudCoilNoW.Value), chkCoilValue .Checked);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            bool[] readCoils = modbusClient.ReadCoils(Decimal.ToInt32(nudCoil1.Value), 1);
+            bool[] readCoils2 = modbusClient.ReadCoils(Decimal.ToInt32(nudCoil2.Value), 1);
+            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(Decimal.ToInt32(nudReg1.Value), 1);
+            int[] readHoldingRegisters2 = modbusClient.ReadHoldingRegisters(Decimal.ToInt32(nudReg2.Value), 1);
+            int[] readInputRegisters = modbusClient.ReadInputRegisters(0, 1);// đọc analogue input
+          
+            txtGiaTri1.Text = readHoldingRegisters[0].ToString();
+            txtGiaTri2.Text = readHoldingRegisters2[0].ToString();
+            chk1.Checked = readCoils[0];
+            chk2.Checked = readCoils2[0];
+            txtInputReg.Text = readInputRegisters[0].ToString();
+            timer2.Enabled = true;
+        }
+    }
+}
